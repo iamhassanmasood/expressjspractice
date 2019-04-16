3a3971154600fc2077efc78f6c90981b3285ad6e
Hassan Masood <iamhassanmasood@gmail.com>
2019-04-17 01:27:17 +0500

    404

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = express();
 const PORT = 4000;
 
 
-app.use('/', (req, res)=>{
+app.get('/', (req, res)=>{
     res.send("<h1>Hello Home Page </h1>")
 })
-
+app.use((req, res)=>{
+    res.status(404).send("<h1>Error 404 </h1> <br/> <h2> Sorry Dear You are on wrong place in right time</h2>")
+})
 app.listen(PORT, ()=>{
     console.log(`App is running on ${PORT}`)
 })
