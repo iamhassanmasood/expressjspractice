d8199806192d58935841140a83d5542ec2ccc30b
Hassan Masood <iamhassanmasood@gmail.com>
2019-04-17 02:44:31 +0500

    routes separeted

--- app.js
+++ app.js
@@ -1,32 +1,17 @@
 const express = require('express');
 const app = express();
 const bodyParser= require('body-parser');
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 const PORT = 4000;
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/', (req, res)=>{
-    res.send("<h1>Welcome Page </h1>")
+app.use(adminRoutes)
+app.use(shopRoutes)
+app.use((req, res, next)=>{
+    res.status(404).send('<h1>Page Not Found</h1>')
 })
-
-/*
-app.use((req, res)=>{
-    res.status(404).send("<h1>Error 404 </h1> <br/> <h2> Sorry Dear You are on wrong place in right time</h2>")
-})
-*/
-
-
-app.use('/add-prod', (req, res)=>{
-    res.send('<form action="/book" method="POST"><input type="text" name="title" /> <button type="submit" >Add Book</button></form>')
-})
-app.get('/home', (req, res)=>{
-    res.send("<h1>Hello from Home Page </h1>")
-})
-app.use('/book', (req, res, next)=>{
-    console.log(req.body)
-    res.redirect('/')
-})
-
 app.listen(PORT, ()=>{
     console.log(`App is running on ${PORT}`)
 })
---
+++ routes/admin.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/add-product', (req, res)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title" /> <button type="submit" >Add product</button></form>')
+})
+router.get('/home', (req, res)=>{
+    res.send("<h1>Hello from Home Page </h1>")
+})
+router.post('/product', (req, res, next)=>{
+    console.log(req.body)
+    res.redirect('/')
+})
+module.exports= router;
