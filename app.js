66e1ba8333c629978df825c7eb5358fec73f778b
Hassan Masood <iamhassanmasood@gmail.com>
2019-04-17 02:49:17 +0500

    admin path define

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = 4000;
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoutes)
+app.use('/admin',adminRoutes)
 app.use(shopRoutes)
 app.use((req, res, next)=>{
     res.status(404).send('<h1>Page Not Found</h1>')
--- routes/admin.js
+++ routes/admin.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/add-product', (req, res)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title" /> <button type="submit" >Add product</button></form>')
+    res.send('<form action="/add-product" method="POST"><input type="text" name="title" /> <button type="submit" >Add product</button></form>')
 })
 router.get('/home', (req, res)=>{
     res.send("<h1>Hello from Home Page </h1>")
 })
-router.post('/product', (req, res, next)=>{
+router.post('/add-product', (req, res, next)=>{
     console.log(req.body)
     res.redirect('/')
 })
