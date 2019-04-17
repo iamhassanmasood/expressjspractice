beb8367dac7157d546749a841cdb285de726c96a
Hassan Masood <iamhassanmasood@gmail.com>
2019-04-17 22:54:31 +0500

    sendind static files

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path =require('path');
 const bodyParser= require('body-parser');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -7,6 +8,7 @@ const errRoutes = require('./routes/error');
 const PORT = 3000;
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
